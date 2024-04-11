88467bb1cd8a86cf4281aa7ac658b35811accfd0
Mahmoud <mahmoud-h-a@hotmail.com>
2024-04-11 16:34:44 +0400

    Testing.

--- cub3D.c
+++ cub3D.c
@@ -6,13 +6,150 @@
 /*   By: mabdelsa <mabdelsa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 14:21:16 by mabdelsa          #+#    #+#             */
-/*   Updated: 2024/03/30 14:33:14 by mabdelsa         ###   ########.fr       */
+/*   Updated: 2024/04/11 16:28:54 by mabdelsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-int main()
-{
+// void place_images(t_mlx_data *mlx_data)
+// {
+//     int x;
+//     int y;
+
+//     y = 0;
+
+//     while (y < mlx_data->map_height)
+//     {
+//         x = 0;
+//         while (x < mlx_data->map_width)
+//         {
+            
+//             if (mlx_data->map[y][x] == '0')
+//                 mlx_put_image_to_window(mlx_data->mlx_ptr, mlx_data->win_ptr, mlx_data->textures[0],
+// 			x * (mlx_data->img_width), y * (mlx_data->img_height));
+//             else if (mlx_data->map[y][x] == '1')
+//                 mlx_put_image_to_window(mlx_data->mlx_ptr, mlx_data->win_ptr, mlx_data->textures[1],
+// 			x * (mlx_data->img_width), y * (mlx_data->img_height));
+//             x++;
+//         }
+//         y++;
+//     }
+// }
+
+// int	assign_images(t_mlx_data *mlx_data)
+// {
+// 	mlx_data->textures[0] = mlx_xpm_file_to_image(mlx_data->mlx_ptr,
+// 			"./textures/white_square.xpm", &mlx_data->img_width, &mlx_data->img_height);
+// 	mlx_data->textures[1] = mlx_xpm_file_to_image(mlx_data->mlx_ptr,
+// 			"./textures/black_square.xpm", &mlx_data->img_width, &mlx_data->img_height);
+// 	if (mlx_data->textures[0] == NULL || mlx_data->textures[1] == NULL)
+// 		return (1);
+// 	return (0);
+// }
+
+
+// int	destroy_window(t_mlx_data *mlx_data)
+// {
+// 	mlx_destroy_window(mlx_data->mlx_ptr, mlx_data->win_ptr);
+// 	exit(0);
+// }
+
+// int	perform_action(int keycode, t_mlx_data *mlx_data)
+// {
+// 	if (keycode == 53)
+// 		destroy_window(mlx_data);
+// 	return (0);
+// }
+
+// void	create_minimap(t_mlx_data *mlx_data)
+// {
+// 	mlx_data->mlx_ptr = mlx_init();
+// 	if (mlx_data->mlx_ptr == NULL)
+// 		return ;
+// 	mlx_data->win_ptr = mlx_new_window(mlx_data->mlx_ptr, mlx_data->map_width * mlx_data->img_width, 
+//         mlx_data->map_height * mlx_data->img_height, "Cub3d");
+// 	if (mlx_data->win_ptr == NULL)
+// 		return ;
+//      place_images(mlx_data);    
+// 	mlx_hook(mlx_data->win_ptr, 17, 1L << 17, destroy_window, mlx_data);
+// 	mlx_hook(mlx_data->win_ptr, 2, 1L << 0, perform_action, mlx_data);
+//     mlx_clear_window(mlx_data->mlx_ptr, mlx_data->win_ptr);
+// 	mlx_loop(mlx_data->mlx_ptr);
+// }
+
+// void init_map(t_mlx_data *mlx_data) {
+//     char map_init[5][5] = {
+//             {'1', '0', '0', '0', '1'},
+//             {'1', '0', '1', '0', '1'},
+//             {'1', '1', '1', '0', '0'},
+//             {'0', '0', '0', '1', '1'},
+//             {'1', '1', '0', '0', '1'}
+//         };
+
+//     // Copy the values to the map
+//     for (int i = 0; i < 5; i++) {
+//         for (int j = 0; j < 5; j++) {
+//             mlx_data->map[i][j] = map_init[i][j];
+//         }
+//     }
+
+   
+//     // Set map width and height
+//     // map->map_width = 5;
+//     // map->map_height = 5;
+// }
+
+
+
+
+// int	main(void)
+// {
+// 	t_mlx_data mlx_data;
+//     assign_images(&mlx_data);
+//     mlx_data.map_height = 5;
+// 	mlx_data.map_width = 5;
+//     mlx_data.img_height = 64;
+// 	mlx_data.img_width = 64;
+//     init_map(&mlx_data);
+
+// 	create_minimap(&mlx_data);
+// }
+
+void draw_square(void *mlx_ptr, void *win_ptr, int x, int y, int size) {
+    int i, j;
+    for (i = x; i < x + size; i++) {
+        for (j = y; j < y + size; j++) {
+            mlx_pixel_put(mlx_ptr, win_ptr, i, j, 0xFF0000); // Red color in RGB format
+        }
+    }
+}
+
+void move_square(void *mlx_ptr, void *win_ptr) {
+    int x = 0;
+    int y = 0;
     
+    while (x + SQUARE_SIZE <= WIN_WIDTH && y + SQUARE_SIZE <= WIN_HEIGHT) {
+        mlx_clear_window(mlx_ptr, win_ptr);
+        draw_square(mlx_ptr, win_ptr, x, y, SQUARE_SIZE);
+       usleep(5000);
+        x++;
+        y++;
+    }
+}
+
+
+
+int main() {
+    void *mlx_ptr;
+    void *win_ptr;
+
+    mlx_ptr = mlx_init();
+    win_ptr = mlx_new_window(mlx_ptr, WIN_WIDTH, WIN_HEIGHT, "Moving Square");
+
+    move_square(mlx_ptr, win_ptr);
+
+    mlx_loop(mlx_ptr);
+
+    return 0;
 }
--- cub3D.h
+++ cub3D.h
@@ -6,7 +6,7 @@
 /*   By: mabdelsa <mabdelsa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 14:21:39 by mabdelsa          #+#    #+#             */
-/*   Updated: 2024/03/30 14:31:54 by mabdelsa         ###   ########.fr       */
+/*   Updated: 2024/04/11 16:26:36 by mabdelsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,20 @@
 # include "./libft/libft.h"
 # include "./mlx/mlx.h"
 
+#define WIN_WIDTH 400
+#define WIN_HEIGHT 400
+#define SQUARE_SIZE 50
+
+typedef struct s_mlx_data
+{
+    void	*mlx_ptr;
+	void	*win_ptr;
+    char     map[5][5];
+    int		map_width;
+	int		map_height;
+	int		img_width;
+	int		img_height;
+    void	*textures[2];
+}	t_mlx_data;
 
 #endif
